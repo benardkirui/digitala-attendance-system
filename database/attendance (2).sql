1b98bb7c123293b97a5c77d19dde2caf1a189ae5
BENARD KIRUI <bernadben41@gmail.com>
2019-08-21 12:12:10 +0300

    database

--- database/attendance (2).sql
+++ database/attendance (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Aug 09, 2019 at 10:38 AM
+-- Generation Time: Aug 21, 2019 at 09:11 AM
 -- Server version: 5.7.26
 -- PHP Version: 7.2.18
 
